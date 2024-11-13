349d586d4c118abc53e77b755aed4a6497360054
sunsuking <sunsuking@gmail.com>
2024-11-13 15:31:24 +0900

    Squashed 'portfolio-client/' content from commit 3cb7441
    git-subtree-dir: portfolio-client
    git-subtree-split: 3cb744182ec006510760ca4462b0be89dd26181a

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
+export const metadata: Metadata = {
+  title: "이력서",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko" className={`${pretendard.variable}`}>
+      <body
+        className={`${pretendard.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}

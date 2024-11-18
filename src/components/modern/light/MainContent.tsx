0db0bfb3456688ed61e913b832ab7390cd0b8e61
김도영 <rlaehdud1002@naver.com>
2024-11-18 12:11:09 +0900

    [#1]feat: 다크모드 시 text color 수정, isPrivate 수정

--- src/components/modern/light/MainContent.tsx
+++ src/components/modern/light/MainContent.tsx
@@ -1,13 +1,19 @@
 import { Basic } from '@/types/resume';
+import Image from 'next/image';
+import Private from 'public/Private.png';
 
 interface MainContentProps {
   basic: Basic;
+  isPrivate: boolean;
 }
 
 const MainContent = (props: MainContentProps) => {
   return (
     <div className='flex flex-col whitespace-pre-wrap w-3/5 truncate text-wrap items-start mt-10'>
-      <h2 className='font-semibold text-4xl'>{props.basic.name}</h2>
+      <div className='flex flex-row space-x-3 items-center'>
+        <h2 className='font-semibold text-4xl'>{props.basic.name}</h2>
+        {props.isPrivate && <Image src={Private} alt='공개 여부' width={36} height={36} />}
+      </div>
       <div className='h-[1px] w-full bg-black my-3' />
       <span className='font-normal text-lg'>{props.basic.job}</span>
       <h3 className='mt-4 font-medium text-3xl'>{props.basic.title}</h3>
